7c75665d988351cee3f18949a6c8cebc324cdfb7
yjw1268 <yjw981213@163.com>
2019-02-23 12:02:22 +0800

    V2.0
    JS study

---
+++ assets/js/my.js
@@ -0,0 +1,21 @@
+$(document).ready(function (){
+  $.getJSON("./assets/js/data1.json", function(data){
+    var $jsontip = $("#jsonTip");
+    var strHtml = "测试";
+    //存储数据的变量
+    $jsontip.empty();
+    //清空内容 
+    $.each(data, function (infoIndex, info){
+      strHtml += "格言：" + info["content"] + "<br>";
+      strHtml += "编码：" + info["number"] + "<br>";
+      strHtml += "<hr>" 
+    }) 
+    var x = 2300;
+    var y = 200;
+    var rand = parseInt(Math.random() * (x - y + 1) + y);
+    $("#description").text(data[rand].content);
+    //$jsontip.html(strHtml);
+    //显示处理后的数据
+
+  }) 
+}) 
