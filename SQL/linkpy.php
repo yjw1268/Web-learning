1f739cf59eef6dcda691efe5c95d43cdd50b0f3b
yjw1268 <yjw981213@163.com>
2019-03-04 17:15:06 +0800

    Update linkpy.php

--- SQL/linkpy.php
+++ SQL/linkpy.php
@@ -7,4 +7,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $output=shell_exec("python3 pixiv_rank.py $mode $number");
     echo $output;
 }
+else{
+    echo "<title>Acess denied!</title>";
+    echo "<h1>非法闯入！</h1>";
+    header("Refresh:1;url=../index.html");
+}
 ?>
